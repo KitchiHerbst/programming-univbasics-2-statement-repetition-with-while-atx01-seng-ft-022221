412b5c536ad8f492de37101e444cf7b768243cd5
Triston Herbst <tristonherbst97@gmail.com>
2021-01-04 21:59:34 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,9 @@
 # Write your code here
 count = 10 
-while count >= 0 do
+while count > 0 do
   puts count
   count -= 1
-  break if count == 0 
-  puts "Happy New Year!"
+  
+  
 end
+puts "Happy New Year!"
